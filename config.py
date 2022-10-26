7eb533a24e77a9482d835c3f17488280070852f3
walterpisacco <65618158+walterpisacco@users.noreply.github.com>
2022-10-26 16:26:01 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+class config():
+	def __init__(self):
+		self.servidor = 'desarrollo.aktis.com.ar'
+		#self.servidor = '127.0.0.1'
+		self.base = 'dx_ingresos'
+		self.usuario = 'admin_iothinks'
+		self.password = 'Global*3522'
+		self.pathUpload = 'upload'
+		self.urlApi = 'http://localhost:4001/'
+		self.cliente = '1'
+		
