4c20ba0058cae45e63076611d1611aa2f9598f98
walterpisacco <65618158+walterpisacco@users.noreply.github.com>
2022-10-26 16:26:37 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 class config():
 	def __init__(self):
-		self.servidor = 'desarrollo.aktis.com.ar'
-		#self.servidor = '127.0.0.1'
-		self.base = 'dx_ingresos'
-		self.usuario = 'admin_iothinks'
-		self.password = 'Global*3522'
+		self.servidor = ''
+		self.base = ''
+		self.usuario = ''
+		self.password = ''
 		self.pathUpload = 'upload'
-		self.urlApi = 'http://localhost:4001/'
-		self.cliente = '1'
+		self.cliente = ''
 		
