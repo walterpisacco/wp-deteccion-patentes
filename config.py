b24bd16cd65291781a8aa6abc6d45e153eb5aae3
Walter <wpisacco@gmail.com>
2022-11-01 13:47:31 -0300

    fix seguridad

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 class config():
 	def __init__(self):
-		self.servidor = 'desarrollo.aktis.com.ar'
-		self.base = 'sas_admin'
-		self.usuario = 'admin_iothinks'
-		self.password = 'Global*3522'
+		self.servidor = ''
+		self.base = ''
+		self.usuario = ''
+		self.password = ''
 		self.pathUpload = 'upload'
 		self.cliente = '1'
 		
